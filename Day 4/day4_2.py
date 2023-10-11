3cc202971834c8c67a61bf21b03d3c3614d9ebe7
Nutsubish <147431395+Nutsubish@users.noreply.github.com>
2023-10-11 16:01:14 +0400

    nicks Day4 ye ye

---
+++ Day 4/day4_2.py
@@ -0,0 +1,18 @@
+
+
+print("family")
+
+momsage=38
+
+dadsage=39
+
+myage=13
+
+they_age=((momsage) + (dadsage))
+
+y_that_we_need_to_add=20
+
+print(they_age)
+
+print(they_age+y_that_we_need_to_add)
+
---
+++ Day 4/day4_3.py
@@ -0,0 +1,15 @@
+
+
+name=input("what is yo name?: ")
+age=input("how old is you?: ")
+age2=13
+
+if age2:
+   print("if age is <18")
+   
+if age2 >= 18:
+   print('adult')
+else:
+   print("leave this")
+
+
